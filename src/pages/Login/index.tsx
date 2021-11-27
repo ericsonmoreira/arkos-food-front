45da55866ce972d3b8aee4a804f404fd69ffe7b2
ericsonmoreira <moreira.ericson@gmail.com>
2021-11-27 17:55:14 -0300

    ⚡️ add react hoor forms

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,13 +1,44 @@
-import { Typography } from '@mui/material';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Button, Grid, TextField } from '@mui/material';
+import { useForm } from 'react-hook-form';
 import LoginLaytout from '../layouts/LoginLaytout';
-
+import schema from './schema';
 import { Container } from './styles';
 
+interface LoginFromData {
+  email: string;
+  password: string;
+}
+
 const Login: React.FC = () => {
+  const { handleSubmit } = useForm<LoginFromData>({
+    defaultValues: {
+      email: '',
+      password: ''
+    },
+    resolver: yupResolver(schema)
+  });
+
+  const onSubmit = (data: LoginFromData) => console.log(data);
+
   return (
     <LoginLaytout>
       <Container>
-        <Typography>Form aqui</Typography>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField label="E-mail" variant="outlined" fullWidth />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField label="Senha" variant="outlined" fullWidth />
+            </Grid>
+            <Grid item xs={12}>
+              <Button type="submit" variant="contained">
+                Entrar
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
       </Container>
     </LoginLaytout>
   );
---
+++ src/pages/Login/schema.ts
@@ -0,0 +1,6 @@
+import * as yup from 'yup';
+
+export default yup.object().shape({
+  email: yup.string().email(),
+  password: yup.string().required()
+});
